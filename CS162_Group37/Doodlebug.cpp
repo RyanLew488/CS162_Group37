3c788d6cc1ee181f7f64fd5e8f4a7f744cae48e7
RyanLew488 <Ryanelew@gmail.com>
2018-05-02 13:30:46 -0700

    Started on the Doodlebug class

---
+++ CS162_Group37/Doodlebug.cpp
@@ -0,0 +1,61 @@
+#include "Doodlebug.hpp"
+
+Doodlebug::Doodlebug() 
+{
+
+}
+
+void Doodlebug::move()
+{
+	switch (dir)
+	{
+	case 0: //Up
+		if (yPos == 0)
+		{
+			yPos = boardHeight;
+		}
+		else
+		{
+			yPos--;
+		}
+		break;
+
+	case 1: //Right
+		if (xPos == boardWidth)
+		{
+			xPos = 0;
+		}
+		else
+		{
+			xPos++
+		}
+		break;
+
+	case 2: //Down
+		if (yPos == boardHeight)
+		{
+			yPos = 0;
+		}
+		else
+		{
+			yPos--;
+		}
+		break;
+
+	case 3: //Left
+		if (xPos == 0)
+		{
+			xPos = boardWidth;
+		}
+		else
+		{
+			xPos--;
+		}
+		break;
+	}
+}
+
+void Doodlebug::turn()
+{
+
+}
