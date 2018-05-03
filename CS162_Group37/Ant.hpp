c356d9293e32fdfa65f990cff592e36d024e1c7f
Linda Carey <lhcarey09@gmail.com>
2018-05-02 17:57:26 -0700

    Hey guys, this is an extremely rough draft with logic not quite completely worked out yet. Pseudocode frame just so you know what I have in mind. Please give me advice if anyone has a better design approach or way of thinking about this

--- CS162_Group37/Ant.hpp
+++ CS162_Group37/Ant.hpp
@@ -1,2 +1,22 @@
-#pragma once
-//stuff goes here.
+/*******************************************************************************
+ ** Ant Class
+ **     This is the header file for the ant class and contains all the
+ **     declarations for ant.cpp
+ ******************************************************************************/
+#ifndef ANT_HPP
+#define ANT_HPP
+
+enum Direction {UP, RIGHT, DOWN, LEFT};
+
+class Ant : public Critter 
+{
+    private:
+        Critter*** boardPtr = nullptr;
+
+    public:
+        Ant(int, int, int, Critter***board);
+        virtual void move();
+        virtual void breed();
+};
+
+#endif
