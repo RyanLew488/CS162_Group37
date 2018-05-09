4098e5d7c278fa57dad56d940458a9aa95a9cd35
coonsd <coonsd@oregonstate.edu>
2018-05-08 22:15:40 -0700

    Added main

---
+++ CS162_Group37/main.cpp
@@ -0,0 +1,19 @@
+/*********************************************************************
+** Program name: main.cpp
+** Assignment: Group Project
+** Author: Group 37
+** Date: May 1, 2018
+** 
+**
+*********************************************************************/
+
+#include "board.hpp"
+
+int main()
+{
+    Board predPreyGame;
+
+    predPreyGame.runGame();
+
+    return 0;
+}
