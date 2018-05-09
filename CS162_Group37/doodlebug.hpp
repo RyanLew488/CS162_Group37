bd4f70fc518cf883bd4ad4b45c109b378501be1c
coonsd <coonsd@oregonstate.edu>
2018-05-09 00:10:36 -0700

    Reimplemented setX/YPos() in DB.  Was removed from Critter.

--- CS162_Group37/doodlebug.hpp
+++ CS162_Group37/doodlebug.hpp
@@ -6,6 +6,54 @@ class Doodlebug : public Critter
 private:
 	int lastAte;
 
+	// ADDED THESE BECAUSE THEY WERE REMOVED FROM CRITTER
+	// WRITTEN IN TERMS OF ROW/COL WHERE POSSIBLE TO MINIMIZE CONFUSION
+
+	int colPos;
+	int rowPos;
+
+	void setColPos(int inCol)
+	{
+		colPos = inCol;
+	}
+
+	void setRowPos(int inRow)
+	{
+		rowPos = inRow;
+	}
+
+	void setXPos(int inX)
+	{
+		setColPos(inX);
+	}
+
+	void setYPos(int inY)
+	{
+		setRowPos(inY);
+	}
+
+
+
+	int getColPos()
+	{
+		return colPos;
+	}
+
+	int getRowPos()
+	{
+		return rowPos;
+	}
+
+	int getXPos()
+	{
+		return getColPos();
+	}
+
+	int getYPos()
+	{
+		return getRowPos();
+	}
+
 public:
 	Doodlebug();
 	Doodlebug(int day, int xPos, int yPos);
