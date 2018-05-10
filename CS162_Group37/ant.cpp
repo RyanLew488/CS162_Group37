002782c93c89a51dc6e5bb615da451bac9c243cd
coonsd <coonsd@oregonstate.edu>
2018-05-10 01:52:38 -0700

    Revised according to newer specs

--- CS162_Group37/ant.cpp
+++ CS162_Group37/ant.cpp
@@ -6,69 +6,155 @@
 #include "ant.hpp"
 #include "critter.hpp"
 #include <random>
-#include <vector>
+#include <ctime>
+#include <cstdlib>
 
 
-void Ant::breed(Critter *** boardState ,int currentDay, int* newCoords, int currentRow, int currentCol)
+Ant::Ant(Critter*** boardState, int boardSizeRow, int boardSizeCol, int currentDay) : 
+                    Critter(currentDay, 0, 3, boardSizeRow, boardSizeCol, boardState)
 {
-    if ((currentDay - getDayLastBred()) >= 3)
+    // seed rand generator
+    std::srand(std::time(nullptr));
+}
+
+
+void Ant::move(int currentDay, int* newCoords, int currentRow, int currentCol)
+{
+    // Keep track of when the last attempted move was
+    setDayLastMove(currentDay);
+
+    int newRow = currentRow;
+    int newCol = currentCol;
+
+    // Generate a direction to move
+    // 0 - up; 1 - right; 2 - down; 3 - left;
+    int direction = rand() % 4;
+
+    switch (direction)
+    {
+        case 0:
+            newRow = currentRow - 1;
+            break;
+        case 1:
+            newCol = currentCol + 1;
+            break;
+        case 2:
+            newRow = currentRow + 1;
+            break;
+        case 3:
+            newCol = currentCol - 1;
+            break;
+    }
+
+    const Critter* tempCritter;
+
+    int flagValidSquare = getSquareState(&tempCritter, newRow, newCol);
+
+    // If square is out of bounds or occupied, then don't move
+    if (!flagValidSquare || tempCritter != NULL)
+    {
+        newRow = currentRow;
+        newCol = currentCol;
+    }
+
+    // Tell board where Ant wants to move (or not move)
+    newCoords[0] = newRow;
+    newCoords[1] = newCol;
+
+}
+
+
+void Ant::breed(int currentDay, int* breedCoords, int currentRow, int currentCol)
+{
+    int breedRow;
+    int breedCol;
+
+    bool flagAllDirectionsChecked = 0;
+    bool flagUpChecked = 0;
+    bool flagRightChecked = 0;
+    bool flagDownChecked = 0;
+    bool flagLeftChecked = 0;
+
+    bool breedingSuccessful = 0;
+
+    const Critter* tempCritter;
+    int flagSquareIsInBounds;
+
+    // Loop until either a valid square is found or all direcitons have been checked
+    while (!breedingSuccessful && !flagAllDirectionsChecked)
+    {
+        // Generate a direction to move
+        // 0 - up; 1 - right; 2 - down; 3 - left;
+        int direction = rand() % 4;
+
+        switch (direction)
+        {
+            case 0:
+                flagUpChecked = 1;
+                breedRow = currentRow - 1;
+                break;
+            case 1:
+                flagRightChecked = 1;
+                breedCol = currentCol + 1;
+                break;
+            case 2:
+                flagDownChecked = 1;
+                breedRow = currentRow + 1;
+                break;
+            case 3:
+                flagLeftChecked = 1;
+                breedCol = currentCol - 1;
+                break;
+        }
+
+        flagSquareIsInBounds = getSquareState(&tempCritter, breedRow, breedCol);
+
+        if (flagSquareIsInBounds && tempCritter == NULL)
+        {
+            breedingSuccessful = 1;
+        }
+
+        flagAllDirectionsChecked = flagUpChecked && flagRightChecked && flagDownChecked && flagLeftChecked;
+    }
+
+
+    if (breedingSuccessful)
     {
         setDayLastBred(currentDay);
-        newCoords = getAvailiable(boardState, currentRow, currentCol);
+
+        breedCoords[0] = breedRow;
+        breedCoords[1] = breedCol;
+    }
+    else
+    {
+        breedCoords[0] = currentRow;
+        breedCoords[1] = currentCol;
     }
 }
 
-void Ant::move(Critter *** boardState ,int currentDay, int* newCoords, int currentRow, int currentCol)
+
+// Get the address of the Critter at a square (row, col)
+// returns an int to indicate success
+//      0 - given coords invalid (outside board)
+//      1 - given coords valid (inside board)
+int Ant::getSquareState(const Critter** critterHolder, int inRow, int inCol)
 {
-    newCoords = getAvailiable(boardState, currentRow, currentCol);
-}
+    // If coords valid (inside board)
+    bool rowValid = (inRow >= 0) && (inRow < getBoardSizeRows());
+    bool colValid = (inCol >= 0) && (inCol < getBoardSizeCols());
 
-int * Ant::getAvailiable(Critter *** boardState, int currentRow, int currentCol)
-{
-    int * results = new int[2];
-    results[0] = this->getXPos();
-    results[1] = this->getYPos();
-
-    //first we check how many of the 4 direction are available
-    int count = 0;
-    std::vector <Critter*> v;
-    if(currentRow != 19 && boardState[currentRow+1][currentCol])
+    if (rowValid && colValid)
     {
-        count++;
-        v.push_back(boardState[currentRow+1][currentCol]);
+        // Pass along the address to the Critter at (inRow, inCol)
+        *critterHolder = pointerToBoardState[inRow][inCol];
+        
+        // Communicate that coords were valid
+        return 1;
     }
-    if(currentRow != 0 && boardState[currentRow-1][currentCol])
+    else
     {
-        count++;
-        v.push_back(boardState[currentRow-1][currentCol]);
+        // Communicate that coords were invalid
+        // (outside of board bounds)
+        return 0;
     }
-    if(currentCol != 19 && boardState[currentRow][currentCol+1])
-    {
-        count++;
-        v.push_back(boardState[currentRow][currentCol+1]);
-
-    }
-    if(currentCol != 0 && boardState[currentRow][currentCol-1])
-    {
-        count++;
-        v.push_back(boardState[currentRow][currentCol-1]);
-    }
-
-    if (count > 0)
-    {
-        std::random_device rd;
-        std::mt19937 e{rd()}; // or std::default_random_engine e{rd()};
-        std::uniform_int_distribution<int> dir{0, count-1};
-
-        int direction = dir(e);
-
-        // COMMENTED OUT SO THAT WE CAN COMPILE
-        // ERROR RESULTS BECAUSE getXPos AND getYPos ARE NO LONGER CRITTER METHODS
-        // int x = v.at(direction)->getXPos();
-        // int y = v.at(direction)->getYPos();
-        // results[0] = x;
-        // results[1] = y;
-    }
-
-    return results;
 }
--- CS162_Group37/ant.hpp
+++ CS162_Group37/ant.hpp
@@ -11,57 +11,35 @@
 class Ant : public Critter 
 {
     private:
-        // ADDED THESE BECAUSE THEY WERE REMOVED FROM CRITTER
-        // WRITTEN IN TERMS OF ROW/COL WHERE POSSIBLE TO MINIMIZE CONFUSION
 
-        int colPos;
-        int rowPos;
-
-        void setColPos(int inCol)
-        {
-            colPos = inCol;
-        }
-
-        void setRowPos(int inRow)
-        {
-            rowPos = inRow;
-        }
-
-        int getColPos()
-        {
-            return colPos;
-        }
-
-        int getRowPos()
-        {
-            return rowPos;
-        }
-
-        int getXPos()
-        {
-            return getColPos();
-        }
-
-        int getYPos()
-        {
-            return getRowPos();
-        }
 
     public:
-        // MODIFIED TO MATCH Critter CONSTRUCTOR (int, int, int)
-        // Ant(int day, int xPos, int yPos): Critter(day,0,xPos,yPos,0){};
-        // Ant(int day, int initSpecies, int xPos, int yPos): Critter(day,0,xPos,yPos,0){};
-        Ant(int day, int xPos, int yPos): Critter(day,0,0){};
-        Ant(int day, int initSpecies, int xPos, int yPos): Critter(day,0,0){};
+        // REPLACED -- BELOW
+        // // MODIFIED TO MATCH Critter CONSTRUCTOR (int, int, int)
+        // // Ant(int day, int xPos, int yPos): Critter(day,0,xPos,yPos,0){};
+        // // Ant(int day, int initSpecies, int xPos, int yPos): Critter(day,0,xPos,yPos,0){};
+        // Ant(int day, int xPos, int yPos): Critter(day,0,0){};
+        // Ant(int day, int initSpecies, int xPos, int yPos): Critter(day,0,0){};
 
-        void move(Critter *** boardState ,int currentDay, int* newCoords, int currentRow, int currentCol);
-        void breed(Critter *** boardState ,int currentDay, int* newCoords, int currentRow, int currentCol);
-        int * getAvailiable(Critter *** boardState, int currentRow, int currentCol);
+        // ADDED - ptr to board, board size rows, board size cols
+        // REMOVED - xPos, yPos
+        Ant(Critter*** boardState, int boardSizeRow, int boardSizeCol, int currentDay);
+
+        // REMOVED - don't match Critter method signatures
+        // void move(Critter *** boardState ,int currentDay, int* newCoords, int currentRow, int currentCol);
+        // void breed(Critter *** boardState ,int currentDay, int* newCoords, int currentRow, int currentCol);
+
+        // REMOVED - move and breed have slightly different logic
+        //      plus getSquareState takes over some of what getAvailable was doing
+        // void getAvailiable(int* results);
+
+        // ADDED - to assist getAvailable
+        int getSquareState(const Critter** critterHolder, int inRow, int inCol);
 
         // ADDED TO MATCH virtual void Critter::move
-        void move(int, int*, int, int){};
+        void move(int, int*, int, int);
         
         // ADDED TO MATCH virtual void Critter:breed
-        void breed(int, int*, int, int){};
+        void breed(int, int*, int, int);
 };
 #endif
