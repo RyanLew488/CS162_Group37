d8c8eae8855ef019b709ccfbcda5b16f236582ae
mcfarkar <38804337+mcfarkar@users.noreply.github.com>
2018-05-12 12:04:26 -0700

    updated constructor in db.hpp

--- CS162_Group37/doodlebug.hpp
+++ CS162_Group37/doodlebug.hpp
@@ -55,7 +55,7 @@ private:
 public:
 	Doodlebug(){};
 	Doodlebug(int day, int xPos, int yPos);
-	Doodlebug(Critter*** boardState, int boardSizeRow, int boardSizeCol, int currentDay) : Critter(currentDay, 1, 8, boardSizeRow, boardSizeCol, boardState);
+	Doodlebug(Critter*** boardState, int boardSizeRow, int boardSizeCol, int currentDay);
 	void move(int currentDay, int* newCoords, int currentRow, int currentCol);
 	void breed(int currentDay, int* newCoords, int currentRow, int currentCol);
 	void setLastAte(int lastAte);
