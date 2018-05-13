8dc0874badcf7dc7b966f11204c3706f5f5d6ec9
mcfarkar <38804337+mcfarkar@users.noreply.github.com>
2018-05-13 12:44:06 -0700

    cleaned up for submission

--- CS162_Group37/ant.hpp
+++ CS162_Group37/ant.hpp
@@ -14,32 +14,12 @@ class Ant : public Critter
 
 
     public:
-        // REPLACED -- BELOW
-        // // MODIFIED TO MATCH Critter CONSTRUCTOR (int, int, int)
-        // // Ant(int day, int xPos, int yPos): Critter(day,0,xPos,yPos,0){};
-        // // Ant(int day, int initSpecies, int xPos, int yPos): Critter(day,0,xPos,yPos,0){};
-        // Ant(int day, int xPos, int yPos): Critter(day,0,0){};
-        // Ant(int day, int initSpecies, int xPos, int yPos): Critter(day,0,0){};
-
-        // ADDED - ptr to board, board size rows, board size cols
-        // REMOVED - xPos, yPos
         Ant(Critter*** boardState, int boardSizeRow, int boardSizeCol, int currentDay);
 
-        // REMOVED - don't match Critter method signatures
-        // void move(Critter *** boardState ,int currentDay, int* newCoords, int currentRow, int currentCol);
-        // void breed(Critter *** boardState ,int currentDay, int* newCoords, int currentRow, int currentCol);
-
-        // REMOVED - move and breed have slightly different logic
-        //      plus getSquareState takes over some of what getAvailable was doing
-        // void getAvailiable(int* results);
-
-        // ADDED - to assist getAvailable
         int getSquareState(const Critter** critterHolder, int inRow, int inCol);
 
-        // ADDED TO MATCH virtual void Critter::move
         void move(int, int*, int, int);
         
-        // ADDED TO MATCH virtual void Critter:breed
         void breed(int, int*, int, int);
 };
 #endif
