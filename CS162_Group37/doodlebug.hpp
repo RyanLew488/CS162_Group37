43779617dd7a274dac105a2dec2e1786733b0e59
mcfarkar <38804337+mcfarkar@users.noreply.github.com>
2018-05-13 12:52:40 -0700

    removed unused prototypes

--- CS162_Group37/doodlebug.hpp
+++ CS162_Group37/doodlebug.hpp
@@ -13,46 +13,6 @@ private:
 	int colPos;
 	int rowPos;
 
-	void setColPos(int inCol)
-	{
-		colPos = inCol;
-	}
-
-	void setRowPos(int inRow)
-	{
-		rowPos = inRow;
-	}
-
-	void setXPos(int inX)
-	{
-		setColPos(inX);
-	}
-
-	void setYPos(int inY)
-	{
-		setRowPos(inY);
-	}
-
-	int getColPos()
-	{
-		return colPos;
-	}
-
-	int getRowPos()
-	{
-		return rowPos;
-	}
-
-	int getXPos()
-	{
-		return getColPos();
-	}
-
-	int getYPos()
-	{
-		return getRowPos();
-	}
-
 public:
 	Doodlebug(Critter*** boardState, int boardSizeRow, int boardSizeCol, int currentDay);
 	void move(int currentDay, int* newCoords, int currentRow, int currentCol);
